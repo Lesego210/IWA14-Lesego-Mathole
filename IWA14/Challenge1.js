5c1fe04f3747c6053b9fcdbc6e2d82185e456add
Lesego210 <127973618+Lesego210@users.noreply.github.com>
2023-06-07 14:51:35 +0200

    Add files via upload

---
+++ IWA14/Challenge1.js
@@ -0,0 +1,14 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = () => {
+  console.log(parameter);
+  console.log(parameter);
+};
+
+const hobbyfunction = () => {
+  logTwice(`Hello, ${firstname} (${age}). I love ${hobby}!`);
+};
+
+hobbyFunction();
